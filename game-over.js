1e19b142fc6c244f26f5021131742f0947dc3fdb
WB <amirm79tahmasbi@gmail.com>
2025-08-24 14:21:19 -0400

    GO10

--- game-over.js
+++ game-over.js
@@ -78,7 +78,7 @@ function drawGameOver() {
 
     // Calculate button positions
     const totalButtonWidth = buttonWidth * 3 + buttonGap * 2;
-    const startX = centerX+ 80;
+    const startX = centerX -20;
 
     // Restart button
     const restartButton = {
