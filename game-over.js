a5d1bf1e7b3cba7e472473caa1be6347124df0e8
WB <amirm79tahmasbi@gmail.com>
2025-08-24 13:54:32 -0400

    GO10

--- game-over.js
+++ game-over.js
@@ -44,7 +44,7 @@ function drawGameOver() {
         const recordImgWidth = boardWidth * 0.444; // ~200px at 450px width
         const recordImgHeight = boardHeight * 0.278; // ~250px at 900px height
         const recordX = centerX - recordImgWidth / 2 - 80;
-        const recordY = boardHeight * 0.15; // ~280px from top at 900px height
+        const recordY = boardHeight * 0.13; // ~280px from top at 900px height
         context.drawImage(images.record, recordX, recordY, recordImgWidth, recordImgHeight);
     }
 
